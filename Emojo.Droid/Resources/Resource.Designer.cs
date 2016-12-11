e2be10a1eb1b1f19d03ef0ef5620460d0cf9c07e
Мальцев Роман <rmaltsev@edu.hse.ru>
2016-12-11 07:13:45 +0300

    Android: add login activity

--- Emojo.Droid/Resources/Resource.Designer.cs
+++ Emojo.Droid/Resources/Resource.Designer.cs
@@ -1948,8 +1948,8 @@ namespace Emojo.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060080
-			public const int action0 = 2131099776;
+			// aapt resource value: 0x7f060081
+			public const int action0 = 2131099777;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -1975,8 +1975,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060084
-			public const int action_divider = 2131099780;
+			// aapt resource value: 0x7f060085
+			public const int action_divider = 2131099781;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -1993,8 +1993,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06003c
 			public const int action_mode_close_button = 2131099708;
 			
-			// aapt resource value: 0x7f060091
-			public const int action_settings = 2131099793;
+			// aapt resource value: 0x7f060092
+			public const int action_settings = 2131099794;
 			
 			// aapt resource value: 0x7f06003d
 			public const int activity_chooser_view_content = 2131099709;
@@ -2023,11 +2023,14 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f060069
 			public const int bottom_sheet = 2131099753;
 			
+			// aapt resource value: 0x7f06007f
+			public const int buttonLogin = 2131099775;
+			
 			// aapt resource value: 0x7f060044
 			public const int buttonPanel = 2131099716;
 			
-			// aapt resource value: 0x7f060081
-			public const int cancel_action = 2131099777;
+			// aapt resource value: 0x7f060082
+			public const int cancel_action = 2131099778;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -2041,8 +2044,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f060087
-			public const int chronometer = 2131099783;
+			// aapt resource value: 0x7f060088
+			public const int chronometer = 2131099784;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -2089,8 +2092,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06000e
 			public const int disableHome = 2131099662;
 			
-			// aapt resource value: 0x7f06007f
-			public const int drawer_layout = 2131099775;
+			// aapt resource value: 0x7f060080
+			public const int drawer_layout = 2131099776;
 			
 			// aapt resource value: 0x7f06005c
 			public const int edit_query = 2131099740;
@@ -2098,8 +2101,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f06008c
-			public const int end_padder = 2131099788;
+			// aapt resource value: 0x7f06008d
+			public const int end_padder = 2131099789;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -2146,8 +2149,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f06008b
-			public const int info = 2131099787;
+			// aapt resource value: 0x7f06008c
+			public const int info = 2131099788;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2155,11 +2158,11 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f060085
-			public const int line1 = 2131099781;
+			// aapt resource value: 0x7f060086
+			public const int line1 = 2131099782;
 			
-			// aapt resource value: 0x7f060089
-			public const int line3 = 2131099785;
+			// aapt resource value: 0x7f06008a
+			public const int line3 = 2131099786;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
@@ -2170,8 +2173,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06006b
 			public const int main_content = 2131099755;
 			
-			// aapt resource value: 0x7f060083
-			public const int media_actions = 2131099779;
+			// aapt resource value: 0x7f060084
+			public const int media_actions = 2131099780;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2182,17 +2185,17 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f060016
 			public const int multiply = 2131099670;
 			
+			// aapt resource value: 0x7f060091
+			public const int nav_discussion = 2131099793;
+			
 			// aapt resource value: 0x7f060090
-			public const int nav_discussion = 2131099792;
-			
-			// aapt resource value: 0x7f06008f
-			public const int nav_friends = 2131099791;
-			
-			// aapt resource value: 0x7f06008d
-			public const int nav_home = 2131099789;
+			public const int nav_friends = 2131099792;
 			
 			// aapt resource value: 0x7f06008e
-			public const int nav_messages = 2131099790;
+			public const int nav_home = 2131099790;
+			
+			// aapt resource value: 0x7f06008f
+			public const int nav_messages = 2131099791;
 			
 			// aapt resource value: 0x7f060074
 			public const int navigation_header_container = 2131099764;
@@ -2320,8 +2323,8 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060082
-			public const int status_bar_latest_event_content = 2131099778;
+			// aapt resource value: 0x7f060083
+			public const int status_bar_latest_event_content = 2131099779;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
@@ -2332,20 +2335,20 @@ namespace Emojo.Droid
 			// aapt resource value: 0x7f06007b
 			public const int tabs = 2131099771;
 			
-			// aapt resource value: 0x7f06008a
-			public const int text = 2131099786;
+			// aapt resource value: 0x7f06008b
+			public const int text = 2131099787;
 			
 			// aapt resource value: 0x7f06007e
 			public const int text1 = 2131099774;
 			
-			// aapt resource value: 0x7f060088
-			public const int text2 = 2131099784;
+			// aapt resource value: 0x7f060089
+			public const int text2 = 2131099785;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
 			
-			// aapt resource value: 0x7f060086
-			public const int time = 2131099782;
+			// aapt resource value: 0x7f060087
+			public const int time = 2131099783;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
@@ -2566,43 +2569,46 @@ namespace Emojo.Droid
 			public const int List_Item = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int Main = 2130903084;
+			public const int Login = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int notification_media_action = 2130903085;
+			public const int Main = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int notification_media_cancel_action = 2130903086;
+			public const int notification_media_action = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int notification_template_big_media = 2130903087;
+			public const int notification_media_cancel_action = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int notification_template_big_media_narrow = 2130903088;
+			public const int notification_template_big_media = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int notification_template_lines = 2130903089;
+			public const int notification_template_big_media_narrow = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int notification_template_media = 2130903090;
+			public const int notification_template_lines = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int notification_template_part_chronometer = 2130903091;
+			public const int notification_template_media = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int notification_template_part_time = 2130903092;
+			public const int notification_template_part_chronometer = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int select_dialog_item_material = 2130903093;
+			public const int notification_template_part_time = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int select_dialog_multichoice_material = 2130903094;
+			public const int select_dialog_item_material = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int select_dialog_singlechoice_material = 2130903095;
+			public const int select_dialog_multichoice_material = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int support_simple_spinner_dropdown_item = 2130903096;
+			public const int select_dialog_singlechoice_material = 2130903096;
+			
+			// aapt resource value: 0x7f030039
+			public const int support_simple_spinner_dropdown_item = 2130903097;
 			
 			static Layout()
 			{
